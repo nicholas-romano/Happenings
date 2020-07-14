945b6b77e26135822ed3c97c012ded921b758f10
Jason Vernot <jason.vernot@gmail.com>
2020-07-14 12:11:35 -0400

    boilerplate code upload. authentication working

---
+++ client/src/pages/Feed/Feed.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+
+function Feed() {
+  return <h1>Check out the neighborhood!</h1>;
+}
+
+export default Feed;
