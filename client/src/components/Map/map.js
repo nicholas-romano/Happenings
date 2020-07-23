a242a464d2f0f24124a913c7cbdf402a17e0f6b7
Jason Vernot <jason.vernot@gmail.com>
2020-07-23 18:36:20 -0400

    location search working with new api

--- client/src/components/Map/map.js
+++ client/src/components/Map/map.js
@@ -3,13 +3,13 @@ import React, { useState, useEffect } from "react";
 import geocodeAPI from "../../utils/geocodeAPI";
 
 function Map() {
-  //   const [mapState, setMapState] = useState({
-  //     places: ["Raleigh, NC", "Wilmington, NC"],
-  //   });
+  const [mapState, setMapState] = useState({
+    places: ["Raleigh, NC", "Wilmington, NC"],
+  });
 
-  //   geocodeAPI.getCoords(mapState.places).then((res) => {
-  //     console.log(res.data.results);
-  //   });
+  geocodeAPI.getCoords(mapState.places).then((res) => {
+    console.log(res.data.results);
+  });
 
   return <div></div>;
 }
