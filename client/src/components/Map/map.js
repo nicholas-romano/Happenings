17e26fb66a84c7e23883a25d8a9ce896f6e928d2
Jason Vernot <jason.vernot@gmail.com>
2020-07-22 22:31:08 -0400

    added map component and geocode API search

---
+++ client/src/components/Map/map.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+
+import geocodeAPI from "../../utils/geocodeAPI";
+
+function Map() {
+  //   const [mapState, setMapState] = useState({
+  //     places: ["Raleigh, NC", "Wilmington, NC"],
+  //   });
+
+  //   geocodeAPI.getCoords(mapState.places).then((res) => {
+  //     console.log(res.data.results);
+  //   });
+
+  return <div></div>;
+}
+
+export default Map;
--- client/src/pages/Feed/Feed.js
+++ client/src/pages/Feed/Feed.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import LocationSearch from "../../components/LocationSearch/locSearch";
+import Map from "../../components/Map/map";
 
 function Feed() {
-  return <LocationSearch />;
+  return (
+    <div>
+      <LocationSearch />
+      <Map />
+    </div>
+  );
 }
 
 export default Feed;
