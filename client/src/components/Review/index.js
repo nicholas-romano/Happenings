56ce124d451466a10f0cc5c8572515368f1f4fcb
Nick Romano <njr7romano@yahoo.com>
2020-07-25 13:55:11 -0400

    Settings form almost complete

--- client/src/components/Review/index.js
+++ client/src/components/Review/index.js
@@ -139,7 +139,7 @@ const Review = props => {
         }
         </div>
         <div className="review-button">
-            <a className="button is-link" onClick={showReviewForm}>Write a Review</a>
+            <button className="button is-link" onClick={showReviewForm}>Write a Review</button>
         </div>
         </>
     ) 
--- client/src/pages/Settings.js
+++ client/src/pages/Settings.js
@@ -1,38 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import SettingsInput from '../components/Form/Settings/SettingsInput';
+import ProfileImg from '../components/Form/Settings/ProfileImg';
 import AUTH from '../utils/AUTH';
+import ProfileImage from '../components/Form/Settings/ProfileImg';
 
 const Settings = () => {
 
+    const imageRef = useRef();
+    const newInterest = useRef();
     const [userData, setUserData] = useState({});
+    const [profileImg, setUserProfileImg] = useState('');
+    const [interestList, setInterestList] = useState([]);
 
     const [enableFields, setEnabledFields] = useState({
         firstName: false,
         userName: false,
-        lastName: false
+        lastName: false,
+        profileImg: false
     })
 
     useEffect(() => {
         getUserData();
     }, []);
 
-    useEffect(() => {
-        //console.log('user Data: ', userData);
-    }, [userData]);
+    // useEffect(() => {
+    //     //console.log('user Data: ', userData);
+    // }, [userData]);
 
-    useEffect(() => {
-        //console.log('field enabled: ', enableFields);
-    }, [enableFields]);
+    // useEffect(() => {
+    //     //console.log('field enabled: ', enableFields);
+    // }, [enableFields]);
+
+     useEffect(() => {
+         console.log('interest List: ', interestList);
+     }, [interestList]);
 
     const getUserData = () => {
         AUTH.getUser().then(res => {
-           const { userName, firstName, lastName } = res.data.user;
+           const { userName, firstName, lastName, profileImg } = res.data.user;
            setUserData({ 
                "Username:": userName, 
-               "First Name:": firstName, 
+               "First Name:": firstName,
                "Last Name:": lastName
            });
-
+           if (profileImg !== undefined) {
+            setUserProfileImg(profileImg)
+           } else {
+            setUserProfileImg('')
+           }
         })
         .catch(err => console.log(err));
     }
@@ -43,6 +58,22 @@ const Settings = () => {
         } else {
             setEnabledFields({...enableFields, [field]: false});
         }
+    }
+
+    const selectImgLink = event => {
+        imageRef.current.select();
+    }
+
+    const addToInterests = () => {
+        const interest = newInterest.current.value;
+        if (interest !== '') {
+            setInterestList([...interestList, interest]);
+            newInterest.current.value = '';
+        }
+    }
+
+    const deleteInterest = () => {
+        console.log('delete index');
     }
 
     const userDataEntries = Object.entries(userData);
@@ -68,6 +99,49 @@ const Settings = () => {
                         )
                     })
                 }
+                <ProfileImage 
+                    profileImg={profileImg}
+                    imageRef={imageRef}
+                    enableFields={enableFields}
+                    enableEdit={enableEdit}
+                    selectImgLink={selectImgLink}
+                />
+                <div className="columns">
+                    <div className="column is-one-fifth">
+                        <label className="label">Add Interest:</label>
+                    </div>
+                    <div className="columns is-four-fifths">
+                        <div className="column is-four-fifths">
+                            <div>
+                                <input className="input" type="text"
+                                    placeholder="Add Interest"
+                                    ref={newInterest}
+                                />
+                            </div>
+                            <div className="interestList">
+                                { (interestList.length > 0 ) ?
+                                    <ul>
+                                        {
+                                            interestList.map((interest, index) => {
+                                                index += 1;
+                                                return (<li key={index}>{index}. {interest} 
+                                                            <span className="fas fa-minus-circle delete-icon" onClick={() => deleteInterest(index)}></span>
+                                                        </li>)
+                                            })
+                                        }
+                                    </ul>
+                                : <p className="empty">Your interest list is empty.</p>
+                                }
+                                
+                            </div>
+                        </div>
+                        <div className="column is-one-fifth">
+                            <button onClick={() => addToInterests()} className="button is-link">
+                                <span className="fas fa-plus"></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
         </>
