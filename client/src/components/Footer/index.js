eedcd725db05a369477304a5fd65ace3c74e2f89
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-27 19:15:35 -0400

    Updatindg contact page to resolve a potential security issue on target _blank hrefs

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const styles = {
-    foot: {
-        backgroundColor: 'rgba(220, 171, 107, 1)'
-      }
-}
+  foot: {
+    backgroundColor: "rgba(220, 171, 107, 1)",
+  },
+};
 
 const Footer = () => {
-
-    return(
-        <footer className='footer p-3' style={styles.foot}>
-        <div className='content has-text-centered'>
-          <p>
-            <strong>Happenings</strong> by Eddie, Jason, Nick, Richard, and Tevin.
-          </p>
-        </div>
-      </footer>
-    )
-}
+  return (
+    <footer className="stickyFooter footer p-3" style={styles.foot}>
+      <div className="content has-text-centered">
+        <p>
+          <strong>Happenings</strong> by Eddie, Jason, Nick, Richard, and Tevin.
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
