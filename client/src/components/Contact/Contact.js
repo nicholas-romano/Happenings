00e34a28387d53131fe849a38e5f5f3e840aa157
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-27 08:05:23 -0400

    Adding geoLocation options for reviews in DB

---
+++ client/src/components/Contact/Contact.js
@@ -0,0 +1,16 @@
+// EXS 26th July 2020
+// Initial contact page build
+// This should be a modal popup with links to the following:
+// Users Github
+// Users LinkedIn
+// Users Professional Facebook Page
+
+// We may need a stylesheet to cover a user mousing over an item
+
+import React from "react";
+
+import "./contact.css";
+
+const Contact = () => {
+  console.log("We've opened the Contact Page");
+};
--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,8 @@
 // EXS 18th July 2020 - reference in our schema files for building seeds
 
 module.exports = {
-  LocationEvents: require('./locationEvents'),
-  Places: require('./places'),
-  Reviews: require('./reviews'),
-  User: require('./user'),
-  // UserInfo: require('./userInfo')
-  // Friends: require('./friends'),
-  //QuickInfo: require('./quickInfo'),
-}
+  LocationEvents: require("./locationEvents"),
+  Places: require("./places"),
+  Reviews: require("./reviews"),
+  User: require("./user"),
+};
