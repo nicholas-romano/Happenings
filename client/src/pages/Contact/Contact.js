7259eb9ea1484edc2013280bd89f7d365ab09e5a
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-27 12:27:42 -0400

    Added in  basic list of developers linked to their github page on contacts

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -9,7 +9,36 @@ const Contact = () => {
   return (
     <>
       <Header />
+      <hr></hr>
       <h1 className="contactTitle">About and Contacts</h1>
+      <hr></hr>
+      <ul>
+        <li>
+          <a href="https://github.com/RichardKessler" target="_blank">
+            Richard Kessler
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/nicholas-romano" target="_blank">
+            Nicholas Romano
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/SaundersEddie" target="_blank">
+            Eddie Saunders
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/jvernot" target="_blank">
+            Jason Vernot
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/TevinWard7" target="_blank">
+            Tevin Ward
+          </a>
+        </li>
+      </ul>
       <hr></hr>
       <Footer />
     </>
