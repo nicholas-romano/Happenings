25625572dd5a01ca59a213356eb476dcae59fd08
Eddie Saunders <saunders.eddie@outlook.com>
2020-07-29 06:52:11 -0400

    Added in icons and links to contacts page. Removed call to mongojs as noted in issue #90

--- server/controllers/reviewsController.js
+++ server/controllers/reviewsController.js
@@ -1,92 +1,109 @@
-const ObjectId = require('mongoose').Types.ObjectId;
-const mongojs = require("mongojs");
-const db = require('../models');
+const ObjectId = require("mongoose").Types.ObjectId;
+// const mongojs = require("mongojs"); //
+const db = require("../models");
 
 // Defining methods for the reviewsController
 module.exports = {
-
-    findAll: (req, res) => {
-        if (req.user) {
-            db.Reviews.find({})
-            .then(reviews => {
-                res.json(reviews);
-            })
-            .catch(err => res.status(422).json(err))
-        }
-    },
-    findById: (req, res) => {
-      console.log('req.params.id: ', req.params.id);
-      if (req.user) {
-        db.Reviews.find({
-          _id: req.params.id
-        }).then(review => {
-          res.json(review);
-        }).catch(err => res.status(422).json(err))
-      }
-    },
-    create: (req, res) => {
-      console.log('review submitted: ', req.body);
-      const { reviewOwner, reviewCreated, reviewTitle, reviewBody, reviewRating, 
-              reviewLocation, reviewLat, reviewLong, reviewGeoLocation, 
-              reviewComments } = req.body;
-        if (req.user) {
-            db.Reviews.create({
-                reviewOwner, 
-                reviewCreated,
-                reviewTitle,
-                reviewBody,
-                reviewRating,
-                reviewLocation,
-                reviewLat,
-                reviewLong,
-                reviewGeoLocation,
-                reviewComments
-            })
-            .then(dbReview => {
-                res.json(dbReview);
-            })
-            .catch(err => res.status(422).json(err))
-        }
-    },
-    addComment: (req, res) => {
-      const { id } = req.params;
-      const {name, user, photo, message, time} = req.body;
-
-      db.Reviews.updateOne(
-          {
-            _id: mongojs.ObjectId(id)
-          }, {
-              $push: {
-                  reviewComments: [{
-                    name, user, photo, message, time
-                  }]
-              }
-          }
-      ).then(comment => {
-          res.json(comment);
-      })
-      .catch(err => {
-          res.json(err);
-      });
-
+  findAll: (req, res) => {
+    if (req.user) {
+      db.Reviews.find({})
+        .then((reviews) => {
+          res.json(reviews);
+        })
+        .catch((err) => res.status(422).json(err));
     }
-//   update: function (req, res) {
-//     db.Reviews.findOneAndUpdate({ _id: req.params.id }, req.body)
-//       .then(dbModel => {
-//         console.log(dbModel)
-//         res.json(dbModel)
-//       })
-//       .catch(err => res.status(422).json(err))
-//   },
-//   remove: function (req, res) {
-//     db.Reviews.findOneAndUpdate(
-//       { _id: req.user._id },
-//       { $pull: { books: new ObjectId(req.params.id) } },
-//       { new: true }
-//     ).then(() => {
-//       db.Book.findOneAndDelete({ _id: req.params.id })
-//         .then(dbReview => res.json(dbReview))
-//         .catch(err => res.status(422).json(err))
-//     })
-//   }
-}
+  },
+  findById: (req, res) => {
+    console.log("req.params.id: ", req.params.id);
+    if (req.user) {
+      db.Reviews.find({
+        _id: req.params.id,
+      })
+        .then((review) => {
+          res.json(review);
+        })
+        .catch((err) => res.status(422).json(err));
+    }
+  },
+  create: (req, res) => {
+    console.log("review submitted: ", req.body);
+    const {
+      reviewOwner,
+      reviewCreated,
+      reviewTitle,
+      reviewBody,
+      reviewRating,
+      reviewLocation,
+      reviewLat,
+      reviewLong,
+      reviewGeoLocation,
+      reviewComments,
+    } = req.body;
+    if (req.user) {
+      db.Reviews.create({
+        reviewOwner,
+        reviewCreated,
+        reviewTitle,
+        reviewBody,
+        reviewRating,
+        reviewLocation,
+        reviewLat,
+        reviewLong,
+        reviewGeoLocation,
+        reviewComments,
+      })
+        .then((dbReview) => {
+          res.json(dbReview);
+        })
+        .catch((err) => res.status(422).json(err));
+    }
+  },
+  addComment: (req, res) => {
+    const { id } = req.params;
+    const { name, user, photo, message, time } = req.body;
+
+    db.Reviews.updateOne(
+      {
+        _id: mongojs.ObjectId(id),
+      },
+      {
+        $push: {
+          reviewComments: [
+            {
+              name,
+              user,
+              photo,
+              message,
+              time,
+            },
+          ],
+        },
+      }
+    )
+      .then((comment) => {
+        res.json(comment);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  },
+  //   update: function (req, res) {
+  //     db.Reviews.findOneAndUpdate({ _id: req.params.id }, req.body)
+  //       .then(dbModel => {
+  //         console.log(dbModel)
+  //         res.json(dbModel)
+  //       })
+  //       .catch(err => res.status(422).json(err))
+  //   },
+  //   remove: function (req, res) {
+  //     db.Reviews.findOneAndUpdate(
+  //       { _id: req.user._id },
+  //       { $pull: { books: new ObjectId(req.params.id) } },
+  //       { new: true }
+  //     ).then(() => {
+  //       db.Book.findOneAndDelete({ _id: req.params.id })
+  //         .then(dbReview => res.json(dbReview))
+  //         .catch(err => res.status(422).json(err))
+  //     })
+  //   }
+};
