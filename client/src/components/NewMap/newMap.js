859f846ef567c7611c5a5c4272978f6b57cf033c
Jason Vernot <jason.vernot@gmail.com>
2020-08-01 11:41:28 -0400

    added container around map for responsive resizing

--- client/src/components/NewMap/newMap.js
+++ client/src/components/NewMap/newMap.js
@@ -43,66 +43,68 @@ function NewMap() {
   }, []);
 
   return (
-    <ReactMapGL
-      {...viewport}
-      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      mapStyle="mapbox://styles/jvernot/ckd80ii7w09rn1imr6fomm1zd"
-      onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    >
-      {eventState.reviews.map((post) => (
-        <Marker
-          key={post._id}
-          latitude={post.reviewLat}
-          longitude={post.reviewLong}
-        >
-          <button
-            className="marker-btn"
-            onClick={(event) => {
-              event.preventDefault();
-              setSelectedEvent(post);
+    <div className="container">
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        mapStyle="mapbox://styles/jvernot/ckd80ii7w09rn1imr6fomm1zd"
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      >
+        {eventState.reviews.map((post) => (
+          <Marker
+            key={post._id}
+            latitude={post.reviewLat}
+            longitude={post.reviewLong}
+          >
+            <button
+              className="marker-btn"
+              onClick={(event) => {
+                event.preventDefault();
+                setSelectedEvent(post);
+              }}
+            >
+              <img src="/mapMarkerWhite.png" alt="Location Icon" />
+            </button>
+          </Marker>
+        ))}
+        {selectedEvent ? (
+          <Popup
+            latitude={selectedEvent.reviewLat}
+            longitude={selectedEvent.reviewLong}
+            onClose={() => {
+              setSelectedEvent(null);
             }}
           >
-            <img src="/mapMarkerWhite.png" alt="Location Icon" />
-          </button>
-        </Marker>
-      ))}
-      {selectedEvent ? (
-        <Popup
-          latitude={selectedEvent.reviewLat}
-          longitude={selectedEvent.reviewLong}
-          onClose={() => {
-            setSelectedEvent(null);
-          }}
-        >
-          <div>
-            <div className="card popup-card">
-              <div className="card-content">
-                <div className="media">
-                  <div className="media-left">
-                    <figure className="image is-48x48">
-                      <img
-                        src="https://bulma.io/images/placeholders/96x96.png"
-                        alt="Placeholder image"
-                      />
-                    </figure>
+            <div>
+              <div className="card popup-card">
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-48x48">
+                        <img
+                          src="https://bulma.io/images/placeholders/96x96.png"
+                          alt="Placeholder image"
+                        />
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">{selectedEvent.reviewTitle}</p>
+                      <p class="subtitle is-6">{selectedEvent.reviewOwner}</p>
+                    </div>
                   </div>
-                  <div className="media-content">
-                    <p className="title is-4">{selectedEvent.reviewTitle}</p>
-                    <p class="subtitle is-6">{selectedEvent.reviewOwner}</p>
-                  </div>
-                </div>
 
-                <div className="content">
-                  <p>{selectedEvent.reviewBody}</p>
-                  <br />
-                  <time>{selectedEvent.reviewCreated}</time>
+                  <div className="content">
+                    <p>{selectedEvent.reviewBody}</p>
+                    <br />
+                    <time>{selectedEvent.reviewCreated}</time>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Popup>
-      ) : null}
-    </ReactMapGL>
+          </Popup>
+        ) : null}
+      </ReactMapGL>
+    </div>
   );
 }
 
