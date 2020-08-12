4429b6bbe868df100cade26d4177174e91a7997b
Nick Romano <njr7romano@yahoo.com>
2020-08-12 09:54:54 -0400

    Added dummy data to seed file

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -12,7 +12,64 @@ mongoose.connect(
   }
 );
 
+const usersSeed = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    userName: "jdoe",
+    userEmail: "jdoe@gmail.com",
+    profileImg: "",
+    password: "password",
+    friends: [
+      {
+        userName: "mjones"
+      }
+    ],
+    userInterest: []
+  },
+  {
+    firstName: "Mary",
+    lastName: "Jones",
+    userName: "mjones",
+    userEmail: "mjones@gmail.com",
+    profileImg: "",
+    password: "password",
+    friends: [
+      {
+        userName: "jdoe"
+      }
+    ],
+    userInterest: []
+  },
+];
+
+const reviewsSeed = [
+  {
+    reviewOwner: "John Doe",
+    reviewCreated: Date.now().toLocaleString(),
+    reviewTitle: "Review Title",
+    reviewBody: "Review Message",
+    reviewRating: 3,
+    reviewLocation: "Review Location",
+    reviewLat: 35.96794,
+    reviewLong: -78.54322,
+    reviewGeoLocation: [35.96794, -78.54322],
+    reviewComments: [
+      {
+        user: "mjones",
+        message: "Review comment",
+        time: Date.now().toLocaleString()
+      }
+    ]
+  },
+];
+
+// EXS When we execture the seedsDB, this removes all entries and then insert the records above
 db.User.deleteMany({})
+  .then(() => {
+    console.log("Adding Users: ", usersSeed)
+  })
+  .then(() => db.User.collection.insertMany(usersSeed))
   .then((data) => {
     process.exit(0);
   })
@@ -22,6 +79,10 @@ db.User.deleteMany({})
   });
 
 db.Reviews.deleteMany({})
+  .then(() => {
+    console.log("Adding Reviews: ", reviewsSeed)
+  })
+  .then(() => db.Reviews.collection.insertMany(reviewsSeed))
   .then((data) => {
     process.exit(0);
   })
